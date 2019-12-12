9b5ec6696cd96f558383d1f72424e281b5ea8a2d
zhangbiao <zhangbiao@channelsoft.com>
2019-12-12 15:26:59 +0800

    update welcome

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">{{ name }}</div>
-    <div class="dashboard-text">welcome</div>
+    <h1>欢迎登录管理平台</h1>
+    <div class="dashboard-text">项目介绍：gmanager基于gf框架的管理平台，支持登录、认证、组织机构、用户、角色、菜单、日志</div>
+    <div class="dashboard-text">
+      github后台地址：
+      <a
+        href="https://github.com/goflyfox/gmanager"
+        target="_blank"
+      >https://github.com/goflyfox/gmanager</a>
+    </div>
+    <div class="dashboard-text">
+      gitee 后台地址：
+      <a
+        href="https://gitee.com/goflyfox/gmanager"
+        target="_blank"
+      >https://gitee.com/goflyfox/gmanager</a>
+    </div>
+    <div class="dashboard-text">
+      github前端地址：
+      <a
+        href="https://github.com/goflyfox/gmanager-web"
+        target="_blank"
+      >https://github.com/goflyfox/gmanager-web</a>
+    </div>
+    <div class="dashboard-text">
+      gitee 前端地址：
+      <a
+        href="https://gitee.com/goflyfox/gmanager-web"
+        target="_blank"
+      >https://gitee.com/goflyfox/gmanager-web</a>
+    </div>
   </div>
 </template>
-
-<script>
-import { mapGetters } from 'vuex'
-
-export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
-  }
-}
-</script>
 
 <style lang="scss" scoped>
 .dashboard {
@@ -24,7 +39,7 @@ export default {
     margin: 30px;
   }
   &-text {
-    font-size: 30px;
+    font-size: 18px;
     line-height: 46px;
   }
 }
